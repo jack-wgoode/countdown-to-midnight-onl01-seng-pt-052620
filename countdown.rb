36eb7081eae01befca6b13add1fdd46f685cb9bc
Jack Goode <jack.w.goode@icloud.com>
2020-05-31 01:30:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,14 @@
 #write your code here
 
-def countdown
-  
+def countdown(integer)
+  seconds = integer
+    while seconds >= 0
+     
+     puts "#{seconds}"
+     sleep seconds
+     seconds -= 1
+    
+     
+    end
+    puts "Happy New Year!"
 end
