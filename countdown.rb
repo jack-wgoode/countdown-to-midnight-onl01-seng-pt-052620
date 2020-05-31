1a1ed325b4ae858e8c383db59cae282372cb3166
Jack Goode <jack.w.goode@icloud.com>
2020-05-31 01:53:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,7 +11,7 @@ end
 def countdown_with_sleep(count_integer)
     seconds = count_integer
     while seconds >= 0
-      puts '#{seconds} SECOND(S)!'
+      puts '#{seconds} SECOND (S)!'
       sleep 1
       seconds -= 1
     end
